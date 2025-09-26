fcd94879f06a644532586c6fb1903d539058f9ac
Ken <kenyio55@gmail.com>
2025-09-26 15:39:36 +0800

    added sliding_window api

--- backend/app/api/sliding_window_api.py
+++ backend/app/api/sliding_window_api.py
@@ -1,6 +1,7 @@
-# backend/app/api/sentiment.py
+
 from fastapi import APIRouter
 from pydantic import BaseModel
+import httpx
 from app.core.sliding_window import afinn_sliding_window
 
 router = APIRouter()
@@ -11,10 +12,11 @@ class TextRequest(BaseModel):
 
 # POST /sliding_window_api/sliding_window
 @router.post("/sliding_window")
-def afinn_sliding_windows(request: TextRequest):
+def sliding_window(request: TextRequest):
     #call the function from analyze_sentiment_per_sentence
-    
-
-    results = afinn_sliding_window(request.text)
+    response=httpx.post("http://localhost:8000/sentiment/analyze_sentiment_per_sentence", json={"text": request.text})
+    #print('this is my response:')
+    #print(response.json())
+    results = afinn_sliding_window(response.json())
     #return {"results": [{"sentence": s, "sentiment": sentiment, "score": score} for s, sentiment, score in results]}
     return results
